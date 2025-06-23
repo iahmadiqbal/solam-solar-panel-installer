ac0ad6f360ec347aea7554f25d3d25e6585ba65b
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-06-23 03:57:26 -0700

    added more pages projects,services,ourteam,contact

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./views/home";
 import About from "./views/about";
+import Projects from "./views/projects";
+import Services from "./views/services";
+import OurTeam from "./views/ourteam";
+import Contact from "./views/contact";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="services" element={<Services />} />
+          <Route path="ourteam" element={<OurTeam />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
