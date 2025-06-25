dbcb8058b71aae4fe61a6b7e2f3baa3ec10ef67d
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-06-25 12:14:18 -0700

    removed component from home component

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -5,6 +5,7 @@ import AboutServicesSection from "../../components/home/AboutServicesSection";
 import ExitSection from "../../components/home/ExitSection";
 import EcoEnergySection from "../../components/home/EcoEnergySection";
 import BrandLogoRow from "../../components/home/BrandLogoRow";
+
 const Home: React.FC = () => {
   return (
     <>
